38c75b732646bbb6e263a8214a0108cda3a8d9e8
Brendan Myers <brendan.myers@temporal.io>
2025-10-30 15:00:57 +1100

    fix: remove timer

--- src/worker.ts
+++ src/worker.ts
@@ -2,13 +2,11 @@ import { Worker } from '@temporalio/worker';
 import { getDataConverter } from './data-converter';
 
 async function run() {
-  // @@@SNIPSTART typescript-encryption-worker
   const worker = await Worker.create({
     workflowsPath: require.resolve('./workflows'),
     taskQueue: 'encryption',
     dataConverter: await getDataConverter(),
   });
-  // @@@SNIPEND
 
   await worker.run();
 }
